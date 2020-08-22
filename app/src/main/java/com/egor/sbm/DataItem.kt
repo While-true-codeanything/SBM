0e4a40a1c4bf18303f815badb8a5a50bfd63d11b
While-true-codeanything <pavlov.erg@gmail.com>
2020-08-22 17:54:16 +0300

    Added Empty Adapter, MemoryAccesser

--- app/src/main/java/com/egor/sbm/DataItem.kt
+++ app/src/main/java/com/egor/sbm/DataItem.kt
@@ -1,4 +1,18 @@
 package com.egor.sbm
 
-class DataItem {
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DataItem(var Value2: Int, var Name2: String) {
+    var Value: Int
+    var Name: String
+    var Time: String
+
+    init {
+        Value = Value2
+        Name = Name2
+        val formatForDateNow =
+            SimpleDateFormat("dd.MM.yyyy")
+        Time = formatForDateNow.format(Date())
+    }
 }
---
+++ app/src/main/java/com/egor/sbm/MemoryAccesser.kt
@@ -0,0 +1,53 @@
+package com.egor.sbm
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.google.gson.Gson
+
+class MemoryAccesser(ct: Context) {
+    companion object {
+        val List = "DtList"
+        val File = "Dta"
+    }
+
+    lateinit var accesser: SharedPreferences
+
+    init {
+        try {
+            val masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+            accesser = ct.getSharedPreferences(
+                File,
+                Context.MODE_PRIVATE
+            )
+            accesser = EncryptedSharedPreferences.create(
+                File,
+                masterKeyAlias,
+                ct,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+
+    fun setData(data: ArrayList<DataItem>) {
+        val editor: SharedPreferences.Editor = accesser.edit()
+        val converter = Gson()
+        val json: String = converter.toJson(data)
+        editor.putString(List, json)
+        editor.apply()
+    }
+
+    fun getData(): ArrayList<DataItem> {
+        val gson = Gson()
+        if (accesser.contains(List)) {
+            val json: String = accesser.getString(List, "").toString()
+            val list: ArrayList<DataItem> =
+                gson.fromJson<ArrayList<DataItem>>(json, DataItem::class.java)
+            return list
+        } else return ArrayList()
+    }
+}
